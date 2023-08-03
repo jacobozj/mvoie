11e31558a3a1eea025b216baa0729dced5cfeba0
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2023-08-03 11:19:53 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Welcome to home page</h1>')
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Jacobo Zuluaga Ja'})
